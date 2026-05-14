b5197bb782db934a9b95bb4181a68e9d4419a6ee
shuvokhan3 <shuvoj360@gmail.com>
2026-05-14 17:55:20 +0600

    helper utility module

---
+++ minitorch/testing.py
@@ -0,0 +1,11 @@
+from minitorch.operators import sigmoid, relu
+
+
+"""Testing utilities for MiniTorch."""
+
+def assert_close(a: float, b:float) -> None:
+    """Assert two floats are close within tolerance"""
+    assert abs(a - b) < 1e-2 , f"Values not close: {a} vs {b}"
+
+
+
