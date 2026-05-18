dfd51564364bf97dbeae231252bd3c911b4a4167
shuvokhan3 <shuvoj360@gmail.com>
2026-05-18 14:38:38 +0600

    solve error

--- minitorch/module.py
+++ minitorch/module.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Tuple, Any, Sequence
 class Parameter:
     """A Trainable parameter in a neural network"""
     def __init__(self,value:any):
-        self.value - value
+        self.value = value
     
     @property
     def shape(self):
@@ -22,66 +22,66 @@ class Parameter:
         """Update the parameter value"""
         self.value = value  
 
-    class Module:
-        """Base class for all neural network modules"""
+class Module:
+    """Base class for all neural network modules"""
 
-        def __init__(self):
-            self.__module__ = {}
-            self.__parameters__ = {}
-            self.training = True
+    def __init__(self):
+        self.__modules__ = {}
+        self.__parameters__ = {}
+        self.training = True
         
-        def modules(self) -> Sequence["Module"]:
-            """Return all sub-modules recursively (deepth-first)"""
-            results = []
-            for module in self.__modules.values():
-                results.append(module)
-                results.extend(module.modules())
-            return results
+    def modules(self) -> Sequence["Module"]:
+        """Return all sub-modules recursively (deepth-first)"""
+        results = []
+        for module in self.__modules__.values():
+            results.append(module)
+            results.extend(module.modules())
+        return results
         
-        def train(self):
-            """Set training mode for this module and all sub-modules"""
-            self.training = True
-            for module in self.modules():
-                module.training = True
+    def train(self):
+        """Set training mode for this module and all sub-modules"""
+        self.training = True
+        for module in self.modules():
+            module.training = True
         
-        def evel(self):
-            """Set evaluation mode for this module and all sub-modules"""
-            self.training = False
-            for module in self.modules():
-                module.training = False
+    def eval(self):
+        """Set evaluation mode for this module and all sub-modules"""
+        self.training = False
+        for module in self.modules():
+            module.training = False
         
-        def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
-            """Return all parameters in this module and sub-modules as (name, parameter) tuples"""
-            results = []
+    def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
+        """Return all parameters in this module and sub-modules as (name, parameter) tuples"""
+        results = []
 
-            #Add direct parameters
-            for name, param in self.__parameters__.items():
-                results.append((name, param))
+        #Add direct parameters
+        for name, param in self.__parameters__.items():
+            results.append((name, param))
             
-            #Add parameters from sub-modules with prefixed names
-            for module_name, module in self.__modules__.items():
-                for sub_name, param in module.named_parameters():
-                    results.append((f"{module_name}.{sub_name}", param))
-            return results
+        #Add parameters from sub-modules with prefixed names
+        for module_name, module in self.__modules__.items():
+            for sub_name, param in module.named_parameters():
+                results.append((f"{module_name}.{sub_name}", param))
+        return results
         
-        def parameters(self) -> Sequence[Parameter]:
-            """Return all parameters in this module and sub-modules as a list"""
-            return [param for _, param in self.named_parameters()]
+    def parameters(self) -> Sequence[Parameter]:
+        """Return all parameters in this module and sub-modules as a list"""
+        return [param for _, param in self.named_parameters()]
         
-        def add_parameter(self, name:str, value:Any)-> Parameter:
-            """Add a parameter to this module"""
-            if isinstance(value, Parameter):
-                param = value
-            else:
-                param = Parameter(value)
+    def add_parameter(self, name:str, value:Any)-> Parameter:
+        """Add a parameter to this module"""
+        if isinstance(value, Parameter):
+            param = value
+        else:
+            param = Parameter(value)
 
-            self.__parameters__[name] = param
-            return param
+        self.__parameters__[name] = param
+        return param
         
-        def __setattr__(self, key: str, value: Any):
-            if isinstance(value, Parameter):
-               self._parameters[key] = value
-            elif isinstance(value, Module):
-               self._modules[key] = value
-
-            super().__setattr__(key, value)
+    def __setattr__(self, key: str, val: Parameter) -> None:
+        if isinstance(val, Parameter):
+            self.__dict__["_parameters"][key] = val
+        elif isinstance(val, Module):
+            self.__dict__["_modules"][key] = val
+        else:
+            super().__setattr__(key, val)
