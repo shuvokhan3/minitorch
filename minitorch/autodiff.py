dd7d3360ccf6eff90f41a4b1c31158e64fd458d2
shuvokhan3 <shuvoj360@gmail.com>
2026-06-29 12:12:28 +0600

    implement class Variable and History for a node in the computation graph and records that operation that created a variable

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -1,6 +1,8 @@
 """automatic differentiation utilities for minitorch"""
 
 from typing import Callable, List , Tuple, Any
+from dataclasses import dataclass
+from typing import Optional, Sequence
 
 def central_difference(f: Callable[..., float], *vals: float, arg: int = 0, epsilon: float = 1e-6) -> float:
     """compute numerical derivative of f with respect to argument 1`arg`. Uses central difference: (f(x+h) - f(x-h)) / (2h)
@@ -35,4 +37,50 @@ def central_difference(f: Callable[..., float], *vals: float, arg: int = 0, epsi
 
     return (f_plus - f_minus) / (2 * epsilon)
 
+
+@dataclass
+class Variable:
+    """
+       A node in the computation graph.
+
+    Attributes:
+      history: Record of the operation that created this variable.
+      derivative: Gradient accumulated during backpropagation.
+      name: Optional name for debugging.
+    """
+
+    history: Optional["History"] = None
+    derivative: Optional[float] = None
+    name: Optional[str] = None
+
+    def is_leaf(self) -> bool :
+        """A leaf varible has no history(was not created by an operation). """
+        return self.history is None
+    
+    def is_constant(self) -> bool:
+        """A constant has no history and will not receive gradients."""
+        return self.history is None
+    
+    def requires_grad_(self, requires_grad: bool = True) -> "Variable":
+        "Set whether this variable should track gradients"
+        if requires_grad:
+            self.history = History()
+        else:
+            self.history = None
+        return self
+
+@dataclass
+class History:
+    """records the opeation that created a variable.
+
+    Attributes:
+    last_fn: The function class that created this varible 
+    ctx: context object storing values needed for backward 
+    inputs: The input variables to the operation
+
+    """
+
+    last_fn: Optional[type] = None
+    ctx: Optional["Context"] = None
+    inputs: Sequence["Variable"] = ()
 
