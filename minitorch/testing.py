e7e778a5e0231c3f1b41f95ed59f5a50b55f079c
shuvokhan3 <shuvoj360@gmail.com>
2026-07-01 11:01:07 +0600

    create a temporary testing file

--- minitorch/testing.py
+++ minitorch/testing.py
@@ -1,12 +1,48 @@
-from minitorch.operators import sigmoid, relu
+from minitorch.scalar import Scalar
+
+# def square(x):
+#     return x * x
+
+# def mul(x, y):
+#     return x * y
+
+# val = central_difference(square, 3.0)
+# print(val)
+
+# val2 = central_difference(mul , 3.0, 4.0 ,arg=0)
+
+# print(val2)
+# print(central_difference(mul, 3.0, 4.0, arg=1))  # ∂(xy)/∂y at (3,4)
 
 
-"""Testing utilities for MiniTorch."""
 
-def assert_close(a: float, b:float) -> None:
-    """Assert two floats are close within tolerance"""
-    assert abs(a - b) < 1e-2 , f"Values not close: {a} vs {b}"
+# a = Scalar(2.0, name="a")
+# b = Scalar(3.0, name="b")
 
 
+# print(f"a.data = {a.data}")
+# print(f"a.is_leaf() = {a.is_leaf()}")
+# print(f"a.derivative = {a.derivative}")
+
+# a.accumulate_derivative(1.0)
+# a.accumulate_derivative(2.0)
+# print(f"After accumulating: a.derivative = {a.derivative}")
+
+# print(a.requires_grad_())
+# print(a.is_leaf())
+
+# a.zero_grad_()
+# print(a.derivative)
 
 
+
+a = Scalar(2.0)
+a.requires_grad_(True)
+b = Scalar(3.0)
+b.requires_grad_(True)
+
+c = a * b  # c = 6.0
+
+print(f"c.data = {c.data}")
+print(f"c.is_leaf() = {c.is_leaf()}")
+print(f"c.history.last_fn = {c.history.last_fn}")
